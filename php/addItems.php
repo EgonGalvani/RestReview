c005f7ed556584f93a3e813cece8f193a4bee929
Alessandro Discalzi <alessandrodiscalzi98@gmail.com>
2019-12-16 15:29:43 +0100

    Modificato additems.php in modo che funzioni con le var di sessione, active del menu da sistemare una volta che viene aggiunto

--- php/addItems.php
+++ php/addItems.php
@@ -4,8 +4,36 @@
         //Aggiunge menu
         $menu = file_get_contents("../components/menu.html");
         $file_content = file_get_contents($subjectPath);
-        //bisogna aggiungere la parte del menu per l'amministratore
+        //Aggiunta menu generico
         $menu_added=str_replace("%MENU%",$menu,$file_content);
+        if(isset($_SESSION['permesso'])){
+            //Aggiunta menu admin
+            if($_SESSION['permesso']==="Admin"){
+                $toAdd="
+                <li class=\"active\" xml:lang=\"en\">Home</li>
+                <li><a href=\"../php/pannello_amministratore.php\">Pannello amministratore</a></li>
+                ";
+                $menu_added=str_replace("<li class=\"active\" xml:lang=\"en\">Home</li>",$toAdd,$menu_added);
+            }
+            //Aggiunta menu ristoratore
+            if($_SESSION['permesso']==="Ristoratore"){
+                $toAdd="
+                <li class=\"active\" xml:lang=\"en\">Home</li>
+                <li><a href=\"../php/mio_profilo.php\">Il mio profilo</a></li>
+                <li><a href=\"../php/miei_ristoranti.php\">I miei ristoranti</a></li>
+                ";
+                $menu_added=str_replace("<li class=\"active\" xml:lang=\"en\">Home</li>",$toAdd,$menu_added);
+            }
+            //Aggiunta menu utente loggato
+            if($_SESSION['permesso']==="Utente"){
+                $toAdd="
+                <li class=\"active\" xml:lang=\"en\">Home</li>
+                <li><a href=\"../php/mio_profilo.php\">Il mio profilo</a></li>
+                <li><a href=\"../php/mie_recensioni.php\">Le mie recensioni</a></li>
+                ";
+                $menu_added=str_replace("<li class=\"active\" xml:lang=\"en\">Home</li>",$toAdd,$menu_added);
+            }
+        }
         
         //Aggiunge header
         $header = file_get_contents("../components/header.html");
