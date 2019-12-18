daca123611ed302f4791fa0c8d9eabafac35e310
Matteo Munari <56451700+matteo-munari@users.noreply.github.com>
2019-12-18 15:40:10 +0100

    Corretto login

--- php/login.php
+++ php/login.php
@@ -1,50 +1,68 @@
-<?php
-
-    function error_msg($type,$num){
-        if($type=='email'){
-            switch($num){
-                case 1: return 'email non valida';
-                case 2: return 'email non corretta';
-            }
-        }
-        if($type=='pwd'){
-            switch($num){
-                case 1: return 'inserire la password';
-                case 2: return 'password non corretta';
-            }
-        }
-        return '';
-    }
+<?php 
 
     require_once("sessione.php");
     if($_SESSION['logged']==true){
         header('location:index.php');
         exit();
     }
-    $file_content=file_get_contents('../html/login.html');
 
-    /*Aggiunta header*/
-    $header='<div id="header">
-    <h1 id="title">RestReview</h1>
-    <a id="hamburger" href="#menu" class="a_btn">MENU</a></div>'; 
-    $file_content=str_replace('%HEADER%',$header,$file_content);
+    /*Aggiunta header,menu e footer*/
+    require_once('addItems.php');
+    $page=addItems('../html/login.html');
 
-    /* Aggiunta menu*/
-    require_once('menu_list.php');
-    $menuList=new menuList('visitatore');
-    $file_content=str_replace('%MENU%',$menuList->getHTMLmenu(),$file_content);
+    $email='';
+    $pwd='';
+    $check='';
+    $error='';
+    /* se ci sono valori in _POST cerca di fare il login o stampa errore */
+    if(isset($_POST['email'])){
+        $email=$_POST['email'];
+        if(isset($_POST['password'])){
+            $pwd=$_POST['password'];
+        }
+        if(isset($_POST['remember_me'])){
+            $check='checked';
+        }
 
-    /*Visualizzo eventuali messaggi di errore*/
-    if(isset($_GET['pwd_error']) && $_GET['pwd_error']!=''){
-        $search='<div id="content">';
-        $replace=$search.'<p id="pwd_error_msg">Errore: '.error_msg('pwd',$_GET['pwd_error']).'</p>';
-        $file_content=str_replace($search,$replace,$file_content);
+        /* crea connessione al DB */
+        require_once('connessione.php');
+        $obj_connection = new DBConnection();
+
+        if($obj_connection->create_connection()){
+
+            $email=$obj_connection->escape_str(trim($email));
+            $hashed_pwd=hash("sha256",$obj_connection->escape_str(trim($pwd)));
+
+            if(!$log_query=$obj_connection->queryDB("SELECT * FROM utente WHERE Mail=\"$email\" AND PWD=\"$hashed_pwd\"")){
+                $error="[Le credenziali inserite non sono corrette ciooo]";
+            }else{
+                $_SESSION['logged']=true;
+                $_SESSION['email']=$email;
+                $_SESSION['ID']=$log_query[0]['ID'];
+                $_SESSION['permesso']=$log_query[0]['Permessi'];
+
+                $obj_connection->close_connection();
+                if(isset($_SESSION['prev_page'])){
+                    header('location: '.$_SESSION['prev_page']);
+                exit;
+                }
+                header('location: index.php');
+                exit;
+            }
+            $obj_connection->close_connection();
+
+        }else{
+            $error="[Errore di connesione al database]";
+        }
+
     }
-    if(isset($_GET['email_error']) && $_GET['email_error']!=''){
-        $search='<div id="content">';
-        $replace=$search.'<p id="email_error_msg">Errore: '.error_msg('email',$_GET['email_error']).'</p>';
-        $file_content=str_replace($search,$replace,$file_content);
-    }
 
-    echo $file_content;
+    $error=str_replace("[","<div>",$error);
+    $error=str_replace("]","</div>",$error);
+    $page=str_replace("%ERROR%",$error,$page);
+    $page=str_replace("%VALUE_EMAIL%",$email,$page);
+    $page=str_replace("%VALUE_PASSWORD%",$pwd,$page);
+    $page=str_replace("%CHECKED%",$check,$page);
+
+    echo $page;
 ?>
