e732ca8816f00513acf0bb7ce68511560490d427
Alessandro Discalzi <alessandrodiscalzi98@gmail.com>
2019-12-18 10:21:55 +0100

    sistemato stringhe

--- php/addItems.php
+++ php/addItems.php
@@ -12,29 +12,29 @@
         if(isset($_SESSION['permesso'])){
             //Aggiunta menu admin
             if($_SESSION['permesso']==="Admin"){
-                $toAdd="
-                <li xml:lang=\"en\"><a href=\"../php/index.php\">Home</a></li>
-                <li><a href=\"../php/pannello_amministratore.php\">Pannello amministratore</a></li>
-                ";
-                $file_content=str_replace("<li class=\"active\" xml:lang=\"en\">Home</li>",$toAdd,$file_content);
+                $toAdd='
+                <li xml:lang="en"><a href="../php/index.php">Home</a></li>
+                <li><a href="../php/pannello_amministratore.php">Pannello amministratore</a></li>
+                ';
+                $file_content=str_replace('<li class="active" xml:lang="en">Home</li>',$toAdd,$file_content);
             }
             //Aggiunta menu ristoratore
             if($_SESSION['permesso']==="Ristoratore"){
-                $toAdd="
-                <li xml:lang=\"en\"><a href=\"../php/index.php\">Home</a></li>
-                <li><a href=\"../php/mio_profilo.php\">Il mio profilo</a></li>
-                <li><a href=\"../php/miei_ristoranti.php\">I miei ristoranti</a></li>
-                ";
-                $file_content=str_replace("<li class=\"active\" xml:lang=\"en\">Home</li>",$toAdd,$file_content);
+                $toAdd='
+                <li xml:lang="en"><a href="../php/index.php">Home</a></li>
+                <li><a href="../php/mio_profilo.php">Il mio profilo</a></li>
+                <li><a href="../php/miei_ristoranti.php">I miei ristoranti</a></li>
+                ';
+                $file_content=str_replace('<li class="active" xml:lang="en">Home</li>',$toAdd,$file_content);
             }
             //Aggiunta menu utente loggato
             if($_SESSION['permesso']==="Utente"){
-                $toAdd="
-                <li xml:lang=\"en\"><a href=\"../php/index.php\">Home</a></li>
-                <li><a href=\"../php/mio_profilo.php\">Il mio profilo</a></li>
-                <li><a href=\"../php/mie_recensioni.php\">Le mie recensioni</a></li>
-                ";
-                $file_content=str_replace("<li class=\"active\" xml:lang=\"en\">Home</li>",$toAdd,$file_content);
+                $toAdd='
+                <li xml:lang="en"><a href="../php/index.php">Home</a></li>
+                <li><a href="../php/mio_profilo.php">Il mio profilo</a></li>
+                <li><a href="../php/mie_recensioni.php">Le mie recensioni</a></li>
+                ';
+                $file_content=str_replace('<li class="active" xml:lang="en">Home</li>',$toAdd,$file_content);
             }
         }
         }       
@@ -42,11 +42,11 @@
         //Aggiunge header
         $header = file_get_contents("../components/header.html");
         if(isset($_SESSION['logged'])&&$_SESSION['logged']){            
-            $header=str_replace("<a href=\"../php/login.php\" class=\"a_btn\">LOGIN</a>",
+            $header=str_replace('<a href="../php/login.php" class="a_btn">LOGIN</a>',
                                     "toReplace",$header);
-            $header=str_replace("<a href=\"../php/registrazione.php\" class=\"a_btn\">REGISTRAZIONE</a>",
+            $header=str_replace('<a href="../php/registrazione.php" class="a_btn">REGISTRAZIONE</a>',
                                     "",$header);
-            $header=str_replace("toReplace","<a href=\"../php/logout_script.php\" class=\"a_btn\">LOGOUT</a>",$header);
+            $header=str_replace("toReplace",'<a href="../php/logout_script.php" class="a_btn">LOGOUT</a>',$header);
         }
         if(basename($_SERVER["REQUEST_URI"])=="login.php"){
             $header ='<div id="header">
