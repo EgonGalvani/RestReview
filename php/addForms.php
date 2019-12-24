d31a0dc28fc16f009a39d46761fbb908e3172744
Egon Galvani <egongalvani@libero.it>
2019-12-24 22:43:36 +0100

    aggiunte classi css form al php

--- php/addForms.php
+++ php/addForms.php
@@ -4,48 +4,49 @@
         public $type;
         public $ristID;
 
-        public function __construct($t,$id){
-            $this->type=$t;
-            $this->ristID=$id;
+        public function __construct($t, $id){
+            $this->type = $t;
+            $this->ristID = $id;
         }
 
         public function getForm(){
+            
             switch($this->type){
-
-                case 'Accetta': {
-                    $method='get';
-                    $action='';
-                    $name='';
-                    $value='Accetta';
+                case 'Accetta': 
+                        $method='post';
+                        $action='';
+                        $name='';
+                        $value='Accetta richiesta';
+                        $class='msg_box success_box'; 
                     break;
-                    }
-                case 'Rifiuta':{
-                    $method='get';
-                    $action='';
-                    $name='';
-                    $value='Rifiuta';
+                case 'Rifiuta': 
+                        $method='post';
+                        $action='';
+                        $name='';
+                        $value='Rifiuta richiesta';
+                        $class='msg_box error_box'; 
                     break;
-                    }
-                case 'Dettaglio':{
-                    $method='post';
-                    $action='dettaglioristorante.php';
-                    $name='visitaRist';
-                    $value='Vai al ristorante';
+                case 'Dettaglio':
+                        $method='post';
+                        $action='dettaglioristorante.php';
+                        $name='visitaRist';
+                        $value='Vai al ristorante';
+                        $class=''; 
                     break;
-                    }
-                case 'Elimina':{
-                    $method='post';
-                    $action='';
-                    $name='';
-                    $value='Elimina';
+               case 'Elimina':
+                        $method='post';
+                        $action='';
+                        $name='';
+                        $value='Elimina ristorante';
+                        $class=''; 
                     break;
-                    }
             }
-            $form="<form method=\"$method\" action=\"$action\" class=\"input_btn_form\">
+
+            $form = "<form method=\"$method\" action=\"$action\" class=\"input_btn_form\">
                         <fieldset>
                             <input type=\"hidden\" name=\"id\" value=\"$this->ristID\">
-                            <input type=\"submit\" name=\"$name\" value=\"$value\" class=\"btn\">
-                        </fieldset>
+                            <input type=\"submit\" name=\"$name\" value=\"$value\" class=\"btn $class\">
+                        </fieldset> 
                     </form>";
 
             return $form;
