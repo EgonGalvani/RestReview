77655ff5056cb42c9ce7422e6887cd062f1d302c
Alessandro Discalzi <alessandrodiscalzi98@gmail.com>
2020-01-03 11:40:42 +0100

    fatto elimina profilo

--- php/profilo.php
+++ php/profilo.php
@@ -1,50 +1,50 @@
 <?php
-require_once("sessione.php");
-require_once('addItems.php');
-require_once('connessione.php');
-if(!$_SESSION['logged']){
-    header('location: login.php');
-}
-$page=addItems('../html/profilo.html');
+    require_once("sessione.php");
+    require_once('addItems.php');
+    require_once('connessione.php');
+    if(!$_SESSION['logged']){
+        header('location: access_denied.php');
+    }
+    $page=addItems('../html/profilo.html');
 
-$no_error=true;
-$error='';
-//connessione db
-$obj_connection = new DBConnection();
-if(!$obj_connection->create_connection()){
-    $error="<div class=\"msg_box error_box\">Errore di connessione al database</div>";
-    $no_error=false;
-}
-if($no_error){
-    $queryResult=$obj_connection->connessione->query("SELECT * FROM utente WHERE ID='".$_SESSION['ID']."'");
-    $row=$queryResult->fetch_array(MYSQLI_ASSOC);
-    $page=str_replace("%EMAIL%",$row['Mail'],$page);
-    $page=str_replace("%UTENZA%",$row['Permessi'],$page);
-    $page=str_replace("%NOME%",$row['Nome'],$page);
-    $page=str_replace("%COGNOME%",$row['Cognome'],$page);
-    $page=str_replace("%SESSO%",$row['Sesso'],$page);
-    $page=str_replace("%DATA_NASCITA%",$row['Data_Nascita'],$page);
-    if($row['Permessi']==='Ristoratore'){
-        $page=str_replace("%PARTITA_IVA%",$row['P_IVA'],$page);
-        $page=str_replace("%RAGIONESOCIALE%",$row['Ragione_Sociale'],$page);
+    $no_error=true;
+    $error='';
+    //connessione db
+    $obj_connection = new DBConnection();
+    if(!$obj_connection->create_connection()){
+        $error="<div class=\"msg_box error_box\">Errore di connessione al database</div>";
+        $no_error=false;
     }
-    else{
-        $page=str_replace("<dt>Partita IVA:</dt>","",$page);
-        $page=str_replace("<dd>%PARTITA_IVA%</dd>","",$page);
-        $page=str_replace("<dt>Ragione Sociale: </dt>","",$page);
-        $page=str_replace("<dd>%RAGIONESOCIALE%</dd>","",$page);
-    }
-    //query per img profilo
-    if($row['ID_Foto']){
-        $queryResult=$obj_connection->connessione->query("SELECT * FROM Foto WHERE ID='".$row['ID_Foto']."'");
-        if($queryResult){
-            $row=$queryResult->fetch_array(MYSQLI_ASSOC);
-            $page=str_replace('../img/imgnotfound.jpg',$row['Path'],$page);
+    if($no_error){
+        $queryResult=$obj_connection->connessione->query("SELECT * FROM utente WHERE ID='".$_SESSION['ID']."'");
+        $row=$queryResult->fetch_array(MYSQLI_ASSOC);
+        $page=str_replace("%EMAIL%",$row['Mail'],$page);
+        $page=str_replace("%UTENZA%",$row['Permessi'],$page);
+        $page=str_replace("%NOME%",$row['Nome'],$page);
+        $page=str_replace("%COGNOME%",$row['Cognome'],$page);
+        $page=str_replace("%SESSO%",$row['Sesso'],$page);
+        $page=str_replace("%DATA_NASCITA%",$row['Data_Nascita'],$page);
+        if($row['Permessi']==='Ristoratore'){
+            $page=str_replace("%PARTITA_IVA%",$row['P_IVA'],$page);
+            $page=str_replace("%RAGIONESOCIALE%",$row['Ragione_Sociale'],$page);
         }
-    }   
+        else{
+            $page=str_replace("<dt>Partita IVA:</dt>","",$page);
+            $page=str_replace("<dd>%PARTITA_IVA%</dd>","",$page);
+            $page=str_replace("<dt>Ragione Sociale: </dt>","",$page);
+            $page=str_replace("<dd>%RAGIONESOCIALE%</dd>","",$page);
+        }
+        //query per img profilo
+        if($row['ID_Foto']){
+            $queryResult=$obj_connection->connessione->query("SELECT * FROM Foto WHERE ID='".$row['ID_Foto']."'");
+            if($queryResult){
+                $row=$queryResult->fetch_array(MYSQLI_ASSOC);
+                $page=str_replace('../img/imgnotfound.jpg',$row['Path'],$page);
+            }
+        }   
 
-}
+    }
 
-$page=str_replace("%ERROR%",$error,$page);
-echo $page;
+    $page=str_replace("%ERROR%",$error,$page);
+    echo $page;
 ?>
---
+++ php/rimuovi_profilo.php
@@ -0,0 +1,24 @@
+<?php
+    require_once("sessione.php");
+    require_once('connessione.php');
+    if(!$_SESSION['logged']){
+        header('location: access_denied.php');
+    }
+    $obj_connection = new DBConnection();
+    if(!$obj_connection->create_connection()){
+        $error="<div class=\"msg_box error_box\">Errore di connessione al database, verrai reindirizzato alla home entro qualche secondo</div>";
+        $no_error=false;
+        echo $error;
+        header('refresh:5; url= index.php');
+    }
+    else{
+        $queryResult=$obj_connection->connessione->query("SELECT ID_Foto FROM utente WHERE ID='".$_SESSION['ID']."'");
+        $row=$queryResult->fetch_array(MYSQLI_ASSOC);
+        if($row['ID_Foto']){
+            $obj_connection->connessione->query("DELETE FROM foto WHERE ID='".$row['ID_Foto']."'");
+        }
+        $obj_connection->connessione->query("DELETE FROM utente WHERE ID='".$_SESSION['ID']."'");
+        header('location: logout_script.php');
+        
+    }
+?>
