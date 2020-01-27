2b8a30ad1dc5979fd80ac0dcf83a128046d56a09
Matteo Munari <56451700+matteo-munari@users.noreply.github.com>
2020-01-27 15:32:26 +0100

    Tolto header a access_denied

--- php/access_denied.php
+++ php/access_denied.php
@@ -2,6 +2,6 @@
     header('refresh:5; url= index.php');
     require_once('addItems.php');    
 
-    $page = (new addItems)->add("../html/access_denied.html");
+    $page = (new addItems)->add("../html/access_denied.html",false,false);
     echo $page; 
 ?>
